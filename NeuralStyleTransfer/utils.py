f42740476dc448b2e5aaddfb9cf9f07a651662a9
Lawrence Francis <lawrencedikeu@gmail.com>
2018-05-23 11:22:30 +0100

    amendment utils.py

--- NeuralStyleTransfer/utils.py
+++ NeuralStyleTransfer/utils.py
@@ -24,7 +24,7 @@ class CONFIG:
     IMAGE_WIDTH = 400
     IMAGE_HEIGHT = 300
     COLOR_CHANNELS = 3
-    NOISE_RATIO = 0.2
+    NOISE_RATIO = 0.6
     MEANS = np.array([123.68, 116.779, 103.939]).reshape((1,1,1,3)) 
     VGG_MODEL = cwd+'pretrained-model/imagenet-vgg-verydeep-19.mat' # Pick the VGG 19-layer model by from the paper "Very Deep Convolutional Networks for Large-Scale Image Recognition".
     STYLE_IMAGE = cwd+'images/stone_style.jpg' # Style image to use.

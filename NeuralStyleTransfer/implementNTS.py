1647c6f7cd3b50c9373f07ce67ad2f2576febec5
leloykun <franzlouiscesista@gmail.com>
2018-07-08 00:42:38 +0800

    debugged load_vgg_model()

--- NeuralStyleTransfer/implementNTS.py
+++ NeuralStyleTransfer/implementNTS.py
@@ -188,28 +188,28 @@ def load_vgg_model(path, IMAGE_HEIGHT, IMAGE_WIDTH, pool_type='avg'):
 
     # Constructs the graph model.
     graph = {}
-    graph['input']   = tf.Variable(np.zeros((1, IMAGE_HEIGHT, IMAGE_WIDTH, COLOR_CHANNELS)), dtype = 'float32')
+    graph['input']    = tf.Variable(np.zeros((1, IMAGE_HEIGHT, IMAGE_WIDTH, COLOR_CHANNELS)), dtype = 'float32')
     graph['conv1_1']  = _conv2d_relu(graph['input'], 0, 'conv1_1')
     graph['conv1_2']  = _conv2d_relu(graph['conv1_1'], 2, 'conv1_2')
-    graph['avgpool1'] = _avgpool(graph['conv1_2'])
-    graph['conv2_1']  = _conv2d_relu(graph['avgpool1'], 5, 'conv2_1')
+    graph['pool1']    = _pool(graph['conv1_2'], pool_type=pool_type)
+    graph['conv2_1']  = _conv2d_relu(graph['pool1'], 5, 'conv2_1')
     graph['conv2_2']  = _conv2d_relu(graph['conv2_1'], 7, 'conv2_2')
-    graph['avgpool2'] = _pool(graph['conv2_2'], pool_type=pool_type)
-    graph['conv3_1']  = _conv2d_relu(graph['avgpool2'], 10, 'conv3_1')
+    graph['pool2']    = _pool(graph['conv2_2'], pool_type=pool_type)
+    graph['conv3_1']  = _conv2d_relu(graph['pool2'], 10, 'conv3_1')
     graph['conv3_2']  = _conv2d_relu(graph['conv3_1'], 12, 'conv3_2')
     graph['conv3_3']  = _conv2d_relu(graph['conv3_2'], 14, 'conv3_3')
     graph['conv3_4']  = _conv2d_relu(graph['conv3_3'], 16, 'conv3_4')
-    graph['avgpool3'] = _pool(graph['conv3_4'], pool_type=pool_type)
-    graph['conv4_1']  = _conv2d_relu(graph['avgpool3'], 19, 'conv4_1')
+    graph['pool3']    = _pool(graph['conv3_4'], pool_type=pool_type)
+    graph['conv4_1']  = _conv2d_relu(graph['pool3'], 19, 'conv4_1')
     graph['conv4_2']  = _conv2d_relu(graph['conv4_1'], 21, 'conv4_2')
     graph['conv4_3']  = _conv2d_relu(graph['conv4_2'], 23, 'conv4_3')
     graph['conv4_4']  = _conv2d_relu(graph['conv4_3'], 25, 'conv4_4')
-    graph['avgpool4'] = _pool(graph['conv4_4'], pool_type=pool_type)
-    graph['conv5_1']  = _conv2d_relu(graph['avgpool4'], 28, 'conv5_1')
+    graph['pool4']    = _pool(graph['conv4_4'], pool_type=pool_type)
+    graph['conv5_1']  = _conv2d_relu(graph['pool4'], 28, 'conv5_1')
     graph['conv5_2']  = _conv2d_relu(graph['conv5_1'], 30, 'conv5_2')
     graph['conv5_3']  = _conv2d_relu(graph['conv5_2'], 32, 'conv5_3')
     graph['conv5_4']  = _conv2d_relu(graph['conv5_3'], 34, 'conv5_4')
-    graph['avgpool5'] = _pool(graph['conv5_4'], pool_type=pool_type)
+    graph['pool5']    = _pool(graph['conv5_4'], pool_type=pool_type)
 
     return graph
 
